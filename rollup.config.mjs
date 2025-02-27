e8be81f6f42b8c710e66002c0218d230823fb22c
arifcseru <arifbd13@gmail.com>
2025-02-27 16:45:10 +0600

    rollup downgraded

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,6 @@
 import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-// import { terser } from '@rollup/plugin-terser'; // ✅ Correct import
-
 import terser from '@rollup/plugin-terser';
 
 
