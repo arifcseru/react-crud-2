19a17a8ffdf9cc9e9d5bc725d932a35b0eaf7f4a
arifcseru <arifbd13@gmail.com>
2025-02-27 13:03:18 +0600

    digital watch updated

--- src/DigitalWatch.tsx
+++ src/DigitalWatch.tsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from "react";
 
-const DigitalWatch: React.FC = () => {
+const DigitalWatch = () => {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
     const interval = setInterval(() => setTime(new Date()), 1000);
-    return () => clearInterval(interval);
+    return () => clearInterval(interval); // Cleanup on unmount
   }, []);
 
   return (
-    <div style={{ fontSize: "2rem", fontFamily: "monospace", textAlign: "center" }}>
-      {time.toLocaleTimeString()}
+    <div>
+      <h2>{time.toLocaleTimeString()}</h2>
     </div>
   );
 };
